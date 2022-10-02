1c0d099f0e889317aa7f31cc699fda389566dde1
Harouna Diallo <dev.harouna@gmail.com>
2022-10-02 01:27:28 +0000

    config setup publisher

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nimbasms",
+    version="0.0.1",
+    author="Nimba SMS",
+    description="Python Client Nimba SMS API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=["nimbasms"],
+    install_requires=['requests']
+)
