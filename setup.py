494a5a80ad57245065968a2d6cc7dacf4a649f9e
Harouna Diallo <dev.harouna@gmail.com>
2023-02-01 23:16:10 +0000

    increase for new release

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name="nimbasms",
-    version="1.0.1",
+    version="1.0.2",
     author="Nimba SMS",
     description="Python Client Nimba SMS API",
     author_email='open-source@nimbasms.com',
